b2054587c0a3d6220737ba490208bbe11882819d
JP1901 <95072861+JP1901@users.noreply.github.com>
2022-11-17 13:10:37 -0500

    Add files via upload

---
+++ Ejercicios_Tema1/src/com/nuevopaquete/Solucion_Tema1.java
@@ -0,0 +1,25 @@
+package com.nuevopaquete;
+
+public class Solucion_Tema1 {
+    public static void main(String[] args) {
+        int a = 1;
+        short b = 4;
+        long c = 1231_12412;
+        double d = 2.1;
+        float e = 23.3f;
+        boolean f = false;
+        byte g = 0;
+        char h = 'a';
+        String i = "Arriba Alianza";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+    }
+}
